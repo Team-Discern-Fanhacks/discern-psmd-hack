bfd044af1ae78e1c5d35e39610e88b7062135915
marius david <mariusdavid@laposte.net>
2020-07-08 17:35:36 +0200

    added a nix-ci configuration file.

---
+++ config/nix-ci.mk
@@ -0,0 +1,12 @@
+PLBEDIT = plbEdit
+CP = cp -v --reflink=auto
+MKDIR = mkdir -v
+LUAC = luac
+PYTHON3 = python
+ZIP = zip
+SCRIPT_ENTRY_LIST_TOOL = script_entry_list_tool
+FLOWTOOL = flowtool
+RM = rm -vf
+RMRECUR = rm -rvf
+MDBOOK = mdbook
+INSTALLDIR = noCitraInstallWithWine
