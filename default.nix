1509abb20c2465253cf63f6344809077e6e4d08d
marius david <mariusdavid@laposte.net>
2020-07-09 09:29:32 +0200

    use nix CI

--- default.nix
+++ default.nix
@@ -10,13 +10,14 @@ let
 
 	plbEdit_wine = pkgs.callPackage ./nix/plbedit_wine.nix {};
 
-	discern_builder = pkgs.callPackage ./nix/discern_native.nix {
+	discern_native_builder = pkgs.callPackage ./nix/discern_native.nix {
 		inherit flowtool script_entry_list_tool plbEdit_wine;
 	};
 in {
 	inherit flowtool script_entry_list_tool plbEdit_wine;
 
-	discern_git = discern_builder (builtins.fetchGit ./.);
+	discern_git = discern_native_builder (builtins.fetchGit ./.);
 
-	discern = discern_builder ./.;
+	discern = discern_native_builder ./.;
+
 }
